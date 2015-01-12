783d25491bc30e3e97ae28b06115fa00fd45acd7
bartekcajdler@gmail.com <bartekcajdler@gmail.com>
2015-01-12 22:47:48 +0100

    added websocket

---
+++ CircleWebsocket/Server.Website/server.js
@@ -0,0 +1,12 @@
+﻿var port = 4000;
+
+var app = require("express")();
+var http = require("http").Server(app);
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname  + "/index.html");
+});
+
+http.listen(port, function () {
+    console.log("listening on *:" + port);
+});
---
+++ CircleWebsocket/Server.Websocket/server.js
@@ -0,0 +1,19 @@
+﻿var port = 4000;
+
+var WebSocketServer = require("ws").Server;
+var server = new WebSocketServer({ port: port });
+
+server.broadcast = function (data, client) {
+    var clients = this.clients;
+    for (var i = 0, max = clients.length; i <max; i++) {
+        if (client !== this.clients[i]) {
+            this.clients[i].send(data);
+        }
+    }
+};
+
+server.on("connection", function (client) {
+    client.on("message", function (message) {
+        server.broadcast(message, client);
+    });
+});
